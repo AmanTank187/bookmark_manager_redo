dbad443fb03e8fb81b459d163070ccfee9241d2c
Aman Tank <amantank187@gmail.com>
2021-03-03 13:50:06 +0000

    Creating bookmark objects

--- spec/features/adding_bookmark_spec.rb
+++ spec/features/adding_bookmark_spec.rb
@@ -3,8 +3,9 @@ feature 'Add a new bookmark' do
         connection = PG.connect(dbname: 'bookmark_manager_test')
         visit ('/bookmarks/new')
         fill_in('url', :with => 'http://testbookmark.com')
+        fill_in('title', :with => 'Test Bookmark')
         click_button('Submit')
-
-        expect(page).to have_content 'http://testbookmark.com'
+        visit('/bookmarks')
+        expect(page).to have_link('Test Bookmark', href: 'http://testbookmark.com')
     end 
 end 
