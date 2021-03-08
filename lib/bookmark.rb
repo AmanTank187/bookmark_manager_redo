b75c07df2499efe1fe12646ae07d2191e572f46f
Aman Tank <amantank187@gmail.com>
2021-03-08 12:19:28 +0000

    Added a timestamp in the database to get in the future to show when bookmark was created

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -33,7 +33,7 @@ class Bookmark
 
     def self.create(url:,title:)
           return false unless is_url?(url)
-          result = DatabaseConnection.query("INSERT INTO bookmarks (url, title) VALUES('#{url}', '#{title}') RETURNING id, title, url;")
+          result = DatabaseConnection.query("INSERT INTO bookmarks (url, title) VALUES('#{url}', '#{title}') RETURNING id, title, url")
           Bookmark.new(
             id: result[0]['id'], 
           title: result[0]['title'],
