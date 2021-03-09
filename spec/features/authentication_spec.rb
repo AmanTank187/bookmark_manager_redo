e215ea310df95380f0e3b5a0ca8b631ada30c783
Aman Tank <amantank187@gmail.com>
2021-03-09 12:36:03 +0000

    Authentication for wrong email

--- spec/features/authentication_spec.rb
+++ spec/features/authentication_spec.rb
@@ -1,5 +1,5 @@
 feature 'authentication' do
-    it 'A user can sign in' do
+    scenario 'A user can sign in' do
         User.create(email: 'test@example.com', password: 'password123')
 
         visit '/sessions/new'
@@ -9,4 +9,16 @@ feature 'authentication' do
     
         expect(page).to have_content 'Welcome, test@example.com'
     end 
+
+    scenario 'a user sees an error if they get their email wrong' do
+        User.create(email: 'test@example.com', password: 'password123')
+    
+        visit '/sessions/new'
+        fill_in(:email, with: 'nottherightemail@me.com')
+        fill_in(:password, with: 'password123')
+        click_button('Sign in')
+    
+        expect(page).not_to have_content 'Welcome, test@example.com'
+        expect(page).to have_content 'Please check your email or password.'
+      end
 end 
